1199aefdb142378b2d01852fabd91f1df1e288ca
IndifferentD <indifferentdensity@gmail.com>
2026-03-04 21:40:12 +0500

    status column to the right of row number

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
-
+vim.opt.statuscolumn = '%=%{v:relnum ? v:relnum : v:lnum} %s'
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
