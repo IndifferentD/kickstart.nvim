ce0393a4845b73ce26981c5b2e1fbf8df80d5cb8
IndifferentD <indifferentdensity@gmail.com>
2026-03-06 19:48:30 +0500

    added autosave

---
+++ lua/custom/plugins/autosave.lua
@@ -0,0 +1,20 @@
+return {
+  'okuuva/auto-save.nvim',
+  event = { 'InsertLeave', 'TextChanged' },
+  opts = {
+    enabled = true,
+
+    trigger_events = {
+      immediate_save = { 'BufLeave', 'FocusLost', 'QuitPre', 'VimSuspend' },
+      defer_save = { 'InsertLeave', 'TextChanged' },
+      cancel_deferred_save = { 'InsertEnter' },
+    },
+
+    debounce_delay = 5000,
+
+    condition = function(buf)
+      local fn = vim.fn
+      return fn.getbufvar(buf, '&modifiable') == 1 and fn.getbufvar(buf, '&readonly') == 0 and fn.expand '%' ~= ''
+    end,
+  },
+}
