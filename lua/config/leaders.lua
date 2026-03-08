58af481de7e323106ea18a5505922c10746764dd
IndifferentD <indifferentdensity@gmail.com>
2026-03-08 22:20:37 +0500

    ai refactoring

---
+++ lua/config/leaders.lua
@@ -0,0 +1,6 @@
+-- Set <space> as leader keys. Must happen before plugins are loaded.
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal.
+vim.g.have_nerd_font = false
