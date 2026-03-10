2a0771b8f6cad6bfb9ba5fa84c49d4985b2c8ad7
IndifferentD <indifferentdensity@gmail.com>
2026-03-10 16:30:44 +0500

    fidget log handler

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -2,9 +2,13 @@ return {
   'j-hui/fidget.nvim',
   opts = {
     progress = {
+      lsp = {
+        log_handler = true,
+      },
       display = {
         render_limit = 16,
         done_ttl = 3,
+        skip_history = false,
       },
     },
   },
