2a50b69a60207b44ec975a4a55dc0095c2b1c948
IndifferentD <indifferentdensity@gmail.com>
2026-03-13 19:14:07 +0500

    linting

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+
+      lint.linters_by_ft = {
+        go = { 'golangcilint' },
+      }
+
+      local golangcilint = lint.linters.golangcilint
+      if type(golangcilint) == 'table' then
+        golangcilint.ignore_exitcode = true
+      end
+
+      local lint_augroup = vim.api.nvim_create_augroup('nvim-lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost' }, {
+        group = lint_augroup,
+        callback = function(args)
+          if vim.bo[args.buf].filetype == 'go' then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+}
