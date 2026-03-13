14d15eb2f0871f86743f846380faa844adf5e878
IndifferentD <indifferentdensity@gmail.com>
2026-03-14 00:03:23 +0500

    which-key helix preset

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,9 +2,14 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
   opts = {
+    preset = 'helix',
     -- delay between pressing a key and opening which-key (milliseconds)
     -- this setting is independent of vim.opt.timeoutlen
-    delay = 0,
+    delay = 100,
+    win = {
+      border = 'rounded',
+      padding = { 1, 2 },
+    },
     icons = {
       -- set icon mappings to true if you have a Nerd Font
       mappings = vim.g.have_nerd_font,
