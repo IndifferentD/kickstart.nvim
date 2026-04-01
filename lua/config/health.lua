2f690876a78d9a0abfac323b9904f222ceb24172
IndifferentD <indifferentdensity@gmail.com>
2026-04-01 15:54:30 +0500

    0.12 lsp config cleanup

--- lua/config/health.lua
+++ lua/config/health.lua
@@ -1,14 +1,14 @@
 local check_version = function()
   local verstr = tostring(vim.version())
   if not vim.version.ge then
-    vim.health.error(string.format("Neovim out of date: '%s'. Upgrade to latest stable or nightly", verstr))
+    vim.health.error(string.format("Neovim out of date: '%s'. This config expects Neovim 0.12+", verstr))
     return
   end
 
-  if vim.version.ge(vim.version(), '0.10-dev') then
+  if vim.version.ge(vim.version(), '0.12.0') then
     vim.health.ok(string.format("Neovim version is: '%s'", verstr))
   else
-    vim.health.error(string.format("Neovim out of date: '%s'. Upgrade to latest stable or nightly", verstr))
+    vim.health.error(string.format("Neovim out of date: '%s'. This config expects Neovim 0.12+", verstr))
   end
 end
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'williamboman/mason.nvim', opts = {} },
-      'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       {
@@ -32,11 +31,7 @@ return {
       ---@param bufnr? integer
       ---@return boolean
       local function client_supports_method(client, method, bufnr)
-        if vim.fn.has 'nvim-0.11' == 1 then
-          return client:supports_method(method, bufnr)
-        else
-          return client.supports_method(method, { bufnr = bufnr })
-        end
+        return client:supports_method(method, { bufnr = bufnr })
       end
 
       local function has_highlight_client(bufnr)
@@ -114,7 +109,7 @@ return {
         end
 
         map('<leader>th', function()
-          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr }, { bufnr = bufnr })
         end, '[T]oggle Inlay [H]ints')
       end
 
@@ -290,12 +285,6 @@ return {
         sqls = {
           filetypes = { 'sql', 'mysql' },
         },
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
         vtsls = {
           filetypes = {
             'javascript',
@@ -315,16 +304,11 @@ return {
         vue_ls = {},
 
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
@@ -334,19 +318,6 @@ return {
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
@@ -356,20 +327,14 @@ return {
         'eslint_d', -- Used by nvim-lint for frontend diagnostics
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+
+      for server_name, server in pairs(servers) do
+        -- Merge blink.cmp capabilities into each server before enabling it via
+        -- the Neovim 0.12 LSP API.
+        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+        vim.lsp.config(server_name, server)
+        vim.lsp.enable(server_name)
+      end
     end,
   },
 }
