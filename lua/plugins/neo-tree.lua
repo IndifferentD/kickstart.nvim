8d5c8587f4fa507e084f59db972440d5f27ec846
IndifferentD <indifferentdensity@gmail.com>
2026-04-21 03:06:41 +0500

    neo-tree search-in-folder, disable row numbers

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -14,10 +14,37 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          vim.opt_local.statuscolumn = ''
+        end,
+      },
+    },
     filesystem = {
+      commands = {
+        live_grep_in_node = function(state)
+          local node = state.tree:get_node()
+          if not node or not node.path then
+            return
+          end
+
+          local search_path = node.type == 'directory' and node.path or vim.fs.dirname(node.path)
+          if not search_path or search_path == '' then
+            return
+          end
+
+          require('telescope.builtin').live_grep {
+            search_dirs = { search_path },
+            prompt_title = 'Live Grep in ' .. vim.fn.fnamemodify(search_path, ':~:.'),
+          }
+        end,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['g/'] = { 'live_grep_in_node', nowait = false },
         },
       },
     },
